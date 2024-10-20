c4f5a150e484ec58fb30093f2924786eae5fa477
Hiteesh Reddy <youremail@example.com>
2024-10-19 20:38:13 -0700

    Run astyle on FullAdder.cpp for formatting

--- source/FourBitAdder/intern/FullAdder.cpp
+++ source/FourBitAdder/intern/FullAdder.cpp
@@ -4,17 +4,20 @@
 FullAdder::FullAdder() : sum(0), carryOut(0) {}
 
 // Method to add three bits
-void FullAdder::add(int a, int b, int carryIn) {
+void FullAdder::add(int a, int b, int carryIn)
+{
     sum = a ^ b ^ carryIn;  // XOR for sum
     carryOut = (a & b) | (b & carryIn) | (a & carryIn);  // OR for carry out
 }
 
 // Getter for sum
-int FullAdder::getSum() const {
+int FullAdder::getSum() const
+{
     return sum;
 }
 
 // Getter for carry out
-int FullAdder::getCarryOut() const {
+int FullAdder::getCarryOut() const
+{
     return carryOut;
 }
