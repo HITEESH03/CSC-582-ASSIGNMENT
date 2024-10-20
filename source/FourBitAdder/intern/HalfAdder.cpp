503fcf053a373db0f349f1c6ad7433902b23c441
Hiteesh Reddy <youremail@example.com>
2024-10-19 20:38:19 -0700

    Run astyle on HalfAdder.cpp for formatting

--- source/FourBitAdder/intern/HalfAdder.cpp
+++ source/FourBitAdder/intern/HalfAdder.cpp
@@ -4,17 +4,20 @@
 HalfAdder::HalfAdder() : sum(0), carry(0) {}
 
 // Method to add two bits
-void HalfAdder::add(int a, int b) {
+void HalfAdder::add(int a, int b)
+{
     sum = a ^ b;  // XOR operation for sum
     carry = a & b;  // AND operation for carry
 }
 
 // Getter for sum
-int HalfAdder::getSum() const {
+int HalfAdder::getSum() const
+{
     return sum;
 }
 
 // Getter for carry
-int HalfAdder::getCarry() const {
+int HalfAdder::getCarry() const
+{
     return carry;
 }
